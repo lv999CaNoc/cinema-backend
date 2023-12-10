8b48536dfe196302632209ef52e77326b717cb3c
hduc <anojn750@gmail.com>
2023-12-10 22:56:53 +0700

    implement bill cleanup schedule

---
+++ src/main/java/com/example/cinema/config/BillCleanup.java
@@ -0,0 +1,30 @@
+package com.example.cinema.config;
+
+import com.example.cinema.pojo.entity.Bill;
+import com.example.cinema.pojo.entity.BillStatus;
+import com.example.cinema.repository.BillRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Component
+@EnableScheduling
+@AllArgsConstructor
+public class BillCleanup {
+    private BillRepository billRepository;
+
+    @Scheduled(fixedRate = 2 * 60000)
+    public void billCleanupTask() {
+        System.out.println("BillCleanup: Cleanup Bill running...");
+        LocalDateTime fiveMinutesAgo = LocalDateTime.now().minusMinutes(5);
+        List<Bill> expiredBills = billRepository.findByCreatedTimeBeforeAndStatus(fiveMinutesAgo, BillStatus.PENDING.ordinal());
+        for (Bill bill : expiredBills) {
+            bill.setStatus(BillStatus.CANCEL);
+        }
+        billRepository.saveAll(expiredBills);
+    }
+}
--- src/main/java/com/example/cinema/repository/BillRepository.java
+++ src/main/java/com/example/cinema/repository/BillRepository.java
@@ -2,9 +2,14 @@ package com.example.cinema.repository;
 
 import com.example.cinema.pojo.entity.Bill;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface BillRepository extends JpaRepository<Bill, Long> {
+    @Query(value = "SELECT * FROM bills WHERE created_time < :time AND status = :status", nativeQuery = true)
+    List<Bill> findByCreatedTimeBeforeAndStatus(LocalDateTime time, int status);
+
     List<Bill> getAllByUserId(Long userId);
 }
